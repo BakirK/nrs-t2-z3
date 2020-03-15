2bd54af6f05d6b5b6b60ad0811e13d2e61f0a1d1
Bakir Karovic <bakir.karovic@gmail.com>
2020-03-15 22:51:21 +0100

    dodana metoda za spasavanje znemenitosti

--- src/ba/unsa/etf/rpr/ZnamenitostiController.java
+++ src/ba/unsa/etf/rpr/ZnamenitostiController.java
@@ -2,11 +2,32 @@ package ba.unsa.etf.rpr;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
 
 public class ZnamenitostiController {
     @FXML
     private TextField fieldZnamenitost;
+    private ImageView imageView;
+    private GeografijaDAO dao;
+
+    public Grad getGrad() {
+        return grad;
+    }
+
+    private void setGrad(Grad grad) {
+        this.grad = grad;
+    }
+
+    private Grad grad;
+
+    public ZnamenitostiController(Grad grad) {
+        this.grad = grad;
+        this.dao = GeografijaDAO.getInstance();
+    }
 
     @FXML
     private void clickOdaberi(ActionEvent actionEvent) {
@@ -14,5 +35,14 @@ public class ZnamenitostiController {
 
     @FXML
     private void clickSave(ActionEvent actionEvent) {
+        Znamenitost z = new Znamenitost(-1, fieldZnamenitost.getText(), imageView.getImageURL().toString(), grad.getId());
+        grad.dodajZnamenitost(z);
+        dao.dodajZnamenitost(z);
+        zatvoriProzorPropuhJe();
+    }
+
+    private void zatvoriProzorPropuhJe() {
+        Stage stage = (Stage) fieldZnamenitost.getScene().getWindow();
+        stage.close();
     }
 }
