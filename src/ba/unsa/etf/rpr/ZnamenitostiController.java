27bd0ca52111ba978c896c42eca5ccd242e2d2ba
Bakir Karovic <bakir.karovic@gmail.com>
2020-03-15 12:10:21 +0100

    napravljen izgled fxml

--- src/ba/unsa/etf/rpr/ZnamenitostiController.java
+++ src/ba/unsa/etf/rpr/ZnamenitostiController.java
@@ -1,4 +1,18 @@
 package ba.unsa.etf.rpr;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class ZnamenitostiController {
+    @FXML
+    private TextField fieldZnamenitost;
+
+    @FXML
+    private void clickOdaberi(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void clickSave(ActionEvent actionEvent) {
+    }
 }
