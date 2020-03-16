bf0264ddf6066d08d5fe31a1b71abbacf47705da
Bakir Karovic <bakir.karovic@gmail.com>
2020-03-16 17:31:47 +0100

    implementiran prikaz novog prozora za search datoteka

--- src/ba/unsa/etf/rpr/ZnamenitostiController.java
+++ src/ba/unsa/etf/rpr/ZnamenitostiController.java
@@ -3,7 +3,10 @@ package ba.unsa.etf.rpr;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
@@ -20,6 +23,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Optional;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class ZnamenitostiController {
     @FXML
@@ -47,7 +52,8 @@ public class ZnamenitostiController {
 
     @FXML
     private void clickOdaberi(ActionEvent actionEvent) {
-        TextInputDialog dialog = new TextInputDialog();
+        //zadatak 2
+        /*TextInputDialog dialog = new TextInputDialog();
         dialog.setHeaderText("Unesite punu putanju do slike");
         dialog.setTitle("Unos slike");
         dialog.setContentText("Putanja:");
@@ -70,6 +76,43 @@ public class ZnamenitostiController {
             alert.setHeaderText("Greška sa pronalaženjem slike!");
             alert.setContentText("Putanja nije ispravna?");
             alert.showAndWait();
+        }*/
+
+        //zadatak 3
+        Stage stage = new Stage();
+        Parent root = null;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/pretraga.fxml"));
+            PretragaController pretragaController = new PretragaController();
+            loader.setController(pretragaController);
+            root = loader.load();
+            stage.setTitle("Pretraga datoteke");
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(true);
+            stage.show();
+
+            stage.setOnHiding( event -> {
+                //TODO
+                try {
+                    //Image image = new Image("file:" + result.get());
+                    //BufferedImage img = null;
+                    //img = ImageIO.read(new File(result.get()));
+                    //Image image = SwingFXUtils.toFXImage(img, null);
+                    File f = new File(pretragaController.getPutanja());
+                    Image imageForFile = new Image(f.toURI().toURL().toExternalForm());
+                    imageView.setImage(imageForFile);
+                    path = pretragaController.getPutanja();
+                } catch(Exception e) {
+                    e.printStackTrace();
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Belaj");
+                    alert.setHeaderText("Greška sa pronalaženjem slike!");
+                    alert.setContentText("Putanja nije ispravna?");
+                    alert.showAndWait();
+                }
+            } );
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
