2d02ba39579efa7acf240b71dec21a888d40d1d6
Bakir Karovic <bakir.karovic@gmail.com>
2020-03-16 20:32:39 +0100

    preimenovan paket u nrs

--- src/ba/unsa/etf/nrs/PretragaController.java
+++ src/ba/unsa/etf/nrs/PretragaController.java
@@ -1,21 +1,17 @@
-package ba.unsa.etf.rpr;
+package ba.unsa.etf.nrs;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Cursor;
-import javafx.scene.ImageCursor;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
--- src/ba/unsa/etf/nrs/ZnamenitostiController.java
+++ src/ba/unsa/etf/nrs/ZnamenitostiController.java
@@ -1,15 +1,13 @@
-package ba.unsa.etf.rpr;
+package ba.unsa.etf.nrs;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -18,11 +16,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Optional;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
