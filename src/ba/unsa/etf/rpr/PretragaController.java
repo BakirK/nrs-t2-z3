5547d6bc0f2ab2df8759ab459b6395075b4fb2ba
Bakir Karovic <bakir.karovic@gmail.com>
2020-03-16 17:55:39 +0100

    dodan listener za dupli click za odabir putanje

--- src/ba/unsa/etf/rpr/PretragaController.java
+++ src/ba/unsa/etf/rpr/PretragaController.java
@@ -1,19 +1,44 @@
 package ba.unsa.etf.rpr;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class PretragaController {
-    @FXML
-    private TextField fieldUzorak;
     @FXML
     private Button btnSearch;
     @FXML
     private ListView listViewUzorci;
-    private String putanja = "";
+    @FXML
+    private TextField fieldUzorak;
+    private String putanja;
+
+    public PretragaController() {
+        putanja = "";
+    }
+
+    @FXML
+    public void initialize() {
+        listViewUzorci.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(mouseEvent.getButton().equals(MouseButton.PRIMARY)){
+                    if(mouseEvent.getClickCount() == 2){
+                        System.out.println("Double clicked");
+                        putanja = listViewUzorci.getSelectionModel().getSelectedItem().toString();
+                        zatvoriProzorPropuhJe();
+                    }
+                }
+            }
+        });
+
+    }
 
     @FXML
     private void clickSearch(ActionEvent actionEvent) {
@@ -23,7 +48,8 @@ public class PretragaController {
         return putanja;
     }
 
-    private void setPutanja(String putanja) {
-        this.putanja = putanja;
+    private void zatvoriProzorPropuhJe() {
+        Stage stage = (Stage) listViewUzorci.getScene().getWindow();
+        stage.close();
     }
 }
