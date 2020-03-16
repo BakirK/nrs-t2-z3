3f07ea531cedff3eb9cde36947dc706ebf6fd935
Bakir Karovic <bakir.karovic@gmail.com>
2020-03-16 20:29:55 +0100

    dodani novi podaci za kreiranje tebele

--- baza.db.sql
+++ baza.db.sql
@@ -1,34 +1,50 @@
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "znamenitost" (
-  "id"	INTEGER,
+BEGIN
+  TRANSACTION;
+  CREATE TABLE IF NOT EXISTS "znamenitost"
+  (
+    "id" INTEGER,
 	"naziv"	TEXT,
 	"slika"	TEXT,
 	"grad_id"	INTEGER,
 	PRIMARY KEY("id"),
 	FOREIGN KEY("grad_id") REFERENCES "grad"
-  );
-CREATE TABLE IF NOT EXISTS "grad" (
-  "id"	INTEGER,
+    );
+  CREATE
+  TABLE
+  IF NOT EXISTS "grad"
+  (
+    "id" INTEGER,
 	"naziv"	TEXT,
 	"broj_stanovnika"	INTEGER,
 	"drzava"	INTEGER,
 	"postanski_broj"	INTEGER,
 	FOREIGN KEY("drzava") REFERENCES "drzava",
 	PRIMARY KEY("id")
-  );
-CREATE TABLE IF NOT EXISTS "drzava" (
-  "id"	INTEGER,
+    );
+  CREATE
+  TABLE
+  IF NOT EXISTS "drzava"
+  (
+    "id" INTEGER,
 	"naziv"	TEXT,
 	"glavni_grad"	INTEGER,
 	PRIMARY KEY("id")
-  );
-INSERT INTO "grad" ("id","naziv","broj_stanovnika","drzava","postanski_broj") VALUES (1,'Pariz',2206488,1,71000);
-INSERT INTO "grad" ("id","naziv","broj_stanovnika","drzava","postanski_broj") VALUES (2,'London',15000,2,71000);
-INSERT INTO "grad" ("id","naziv","broj_stanovnika","drzava","postanski_broj") VALUES (3,'Beč',1899055,3,71000);
-INSERT INTO "grad" ("id","naziv","broj_stanovnika","drzava","postanski_broj") VALUES (4,'Manchester',545500,2,71000);
-INSERT INTO "grad" ("id","naziv","broj_stanovnika","drzava","postanski_broj") VALUES (5,'Graz',280200,3,71000);
-INSERT INTO "grad" ("id","naziv","broj_stanovnika","drzava","postanski_broj") VALUES (6,'Sarajevo',350000,3,71000);
-INSERT INTO "drzava" ("id","naziv","glavni_grad") VALUES (1,'Francuska',1);
-INSERT INTO "drzava" ("id","naziv","glavni_grad") VALUES (2,'Velika Britanija',2);
-INSERT INTO "drzava" ("id","naziv","glavni_grad") VALUES (3,'Austrija',3);
-COMMIT;
+    );
+  INSERT INTO "znamenitost" ("id", "naziv", "slika", "grad_id") VALUES (7, ''asas'', '''', 5);
+  INSERT INTO "grad" ("id", "naziv", "broj_stanovnika", "drzava", "postanski_broj")
+  VALUES (1, ''Pariz'', 2206488, 1, 71000);
+  INSERT INTO "grad" ("id", "naziv", "broj_stanovnika", "drzava", "postanski_broj")
+  VALUES (2, ''London'', 15000, 2, 71000);
+  INSERT INTO "grad" ("id", "naziv", "broj_stanovnika", "drzava", "postanski_broj")
+  VALUES (3, ''Beč'', 1899055, 3, 71000);
+  INSERT INTO "grad" ("id", "naziv", "broj_stanovnika", "drzava", "postanski_broj")
+  VALUES (4, ''Manchester'', 545500, 2, 71000);
+  INSERT INTO "grad" ("id", "naziv", "broj_stanovnika", "drzava", "postanski_broj")
+  VALUES (5, ''Graz'', 280200, 3, 71000);
+  INSERT INTO "grad" ("id", "naziv", "broj_stanovnika", "drzava", "postanski_broj")
+  VALUES (6, ''Sarajevo'', 350000, 3, 71000);
+  INSERT INTO "grad" ("id", "naziv", "broj_stanovnika", "drzava", "postanski_broj") VALUES (7, ''aaa'', 555, 2, 71000);
+  INSERT INTO "drzava" ("id", "naziv", "glavni_grad") VALUES (1, ''Francuska'', 1);
+  INSERT INTO "drzava" ("id", "naziv", "glavni_grad") VALUES (2, ''Velika Britanija'', 2);
+  INSERT INTO "drzava" ("id", "naziv", "glavni_grad") VALUES (3, ''Austrija'', 3);
+  COMMIT;
