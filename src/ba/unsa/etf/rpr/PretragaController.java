4dd1a375df1547f4eb2b3d878c22fcf3680e6a62
Bakir Karovic <bakir.karovic@gmail.com>
2020-03-16 17:19:03 +0100

    dodana osnovna polja sa fxml u kontroler

---
+++ src/ba/unsa/etf/rpr/PretragaController.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class PretragaController {
+    @FXML
+    private TextField fieldUzorak;
+    @FXML
+    private Button btnSearch;
+    @FXML
+    private ListView listViewUzorci;
+
+    @FXML
+    private void clickSearch(ActionEvent actionEvent) {
+    }
+}
